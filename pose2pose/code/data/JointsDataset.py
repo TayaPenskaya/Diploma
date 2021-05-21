c576cd133453b9398b468d94baed8aab3b8f40ba
Taya Penskaya <tayaserf@gmail.com>
2021-05-21 17:50:04 +0300

    Added data connected files

---
+++ pose2pose/code/data/JointsDataset.py
@@ -0,0 +1,164 @@
+import copy
+import random
+
+import cv2
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+from utils.transforms import get_affine_transform
+from utils.transforms import affine_transform
+from utils.transforms import fliplr_joints
+
+
+class JointsDataset(Dataset):
+    def __init__(self, ann_file):
+        self.num_joints = 0
+        self.pixel_std = 200
+        self.flip_pairs = []
+        self.parent_ids = []
+
+        self.ann_file = ann_file
+
+        self.image_size = np.array([256, 256])
+        self.sigma = 2
+        self.use_different_joints_weight = 1
+        self.joints_weight = 1
+
+        self.db = []
+
+    def _get_db(self):
+        raise NotImplementedError
+
+    def evaluate(self):
+        raise NotImplementedError
+
+    def __len__(self,):
+        return len(self.db)
+
+    def __getitem__(self, idx):
+        db_rec = copy.deepcopy(self.db[idx])
+
+        image_file = db_rec['image']
+
+        joints = db_rec['joints_2d']
+        joints_vis = db_rec['joints_2d_vis']
+
+        c = db_rec['center']
+        s = db_rec['scale']
+        category = db_rec['category']
+        r = 0
+
+        trans = get_affine_transform(c, s, r, self.image_size)
+
+        for i in range(self.num_joints):
+            if joints_vis[i, 0] > 0.0:
+                joints[i, 0:2] = affine_transform(joints[i, 0:2], trans)
+
+        #target, target_weight = self.generate_target(joints, joints_vis)
+
+        #target = torch.from_numpy(target)
+        #target_weight = torch.from_numpy(target_weight)
+
+        meta = {
+            'image': image_file,
+            'joints': joints,
+            'joints_vis': joints_vis,
+            'center': c,
+            'scale': s,
+            'rotation': r,
+            'category': category
+        }
+
+        return joints, category
+
+    def select_data(self, db):
+        db_selected = []
+        for rec in db:
+            num_vis = 0
+            joints_x = 0.0
+            joints_y = 0.0
+            for joint, joint_vis in zip(
+                    rec['joints_2d'], rec['joints_2d_vis']):
+                if joint_vis[0] <= 0:
+                    continue
+                num_vis += 1
+
+                joints_x += joint[0]
+                joints_y += joint[1]
+            if num_vis == 0:
+                continue
+
+            joints_x, joints_y = joints_x / num_vis, joints_y / num_vis
+
+            area = rec['scale'][0] * rec['scale'][1] * (self.pixel_std**2)
+            joints_center = np.array([joints_x, joints_y])
+            bbox_center = np.array(rec['center'])
+            diff_norm2 = np.linalg.norm((joints_center-bbox_center), 2)
+            ks = np.exp(-1.0*(diff_norm2**2) / ((0.2)**2*2.0*area))
+
+            metric = (0.2 / 16) * num_vis + 0.45 - 0.2 / 16
+            if ks > metric:
+                db_selected.append(rec)
+
+        print('=> num db: {}'.format(len(db)))
+        print('=> num selected db: {}'.format(len(db_selected)))
+        return db_selected
+
+    def generate_target(self, joints, joints_vis):
+        '''
+        :param joints:  [num_joints, 3]
+        :param joints_vis: [num_joints, 3]
+        :return: target, target_weight(1: visible, 0: invisible)
+        '''
+        target_weight = np.ones((self.num_joints, 1), dtype=np.float32)
+        target_weight[:, 0] = joints_vis[:, 0]
+
+        assert self.target_type == 'gaussian', \
+            'Only support gaussian map now!'
+
+        if self.target_type == 'gaussian':
+            target = np.zeros((self.num_joints,
+                               self.heatmap_size[1],
+                               self.heatmap_size[0]),
+                              dtype=np.float32)
+
+            tmp_size = self.sigma * 3
+
+            for joint_id in range(self.num_joints):
+                feat_stride = self.image_size / self.heatmap_size
+                mu_x = int(joints[joint_id][0] / feat_stride[0] + 0.5)
+                mu_y = int(joints[joint_id][1] / feat_stride[1] + 0.5)
+                # Check that any part of the gaussian is in-bounds
+                ul = [int(mu_x - tmp_size), int(mu_y - tmp_size)]
+                br = [int(mu_x + tmp_size + 1), int(mu_y + tmp_size + 1)]
+                if ul[0] >= self.heatmap_size[0] or ul[1] >= self.heatmap_size[1] \
+                        or br[0] < 0 or br[1] < 0:
+                    # If not, just return the image as is
+                    target_weight[joint_id] = 0
+                    continue
+
+                # # Generate gaussian
+                size = 2 * tmp_size + 1
+                x = np.arange(0, size, 1, np.float32)
+                y = x[:, np.newaxis]
+                x0 = y0 = size // 2
+                # The gaussian is not normalized, we want the center value to equal 1
+                g = np.exp(- ((x - x0) ** 2 + (y - y0) ** 2) / (2 * self.sigma ** 2))
+
+                # Usable gaussian range
+                g_x = max(0, -ul[0]), min(br[0], self.heatmap_size[0]) - ul[0]
+                g_y = max(0, -ul[1]), min(br[1], self.heatmap_size[1]) - ul[1]
+                # Image range
+                img_x = max(0, ul[0]), min(br[0], self.heatmap_size[0])
+                img_y = max(0, ul[1]), min(br[1], self.heatmap_size[1])
+
+                v = target_weight[joint_id]
+                if v > 0.5:
+                    target[joint_id][img_y[0]:img_y[1], img_x[0]:img_x[1]] = \
+                        g[g_y[0]:g_y[1], g_x[0]:g_x[1]]
+
+        if self.use_different_joints_weight:
+            target_weight = np.multiply(target_weight, self.joints_weight)
+
+        return target, target_weight
---
+++ pose2pose/code/data/mpii.py
@@ -0,0 +1,114 @@
+import os
+import json
+from collections import OrderedDict
+
+import numpy as np
+from scipy.io import loadmat, savemat
+
+import torch
+
+from data.JointsDataset import JointsDataset
+
+class MPIIDataset(JointsDataset):
+    """MPII Dataset for top-down pose estimation.
+    
+    MPII keypoint indexes::
+        0: 'right_ankle'
+        1: 'right_knee',
+        2: 'right_hip',
+        3: 'left_hip',
+        4: 'left_knee',
+        5: 'left_ankle',
+        6: 'pelvis',
+        7: 'thorax',
+        8: 'upper_neck',
+        9: 'head_top',
+        10: 'right_wrist',
+        11: 'right_elbow',
+        12: 'right_shoulder',
+        13: 'left_shoulder',
+        14: 'left_elbow',
+        15: 'left_wrist'
+    """
+    
+    def __init__(self, ann_file):
+        super().__init__(ann_file)
+
+        self.num_joints = 16
+        self.flip_pairs = [[0, 5], [1, 4], [2, 3], [10, 15], [11, 14], [12, 13]]
+        self.parent_ids = [1, 2, 6, 6, 3, 4, 6, 6, 7, 8, 11, 12, 7, 7, 13, 14]
+
+        self.upper_body_ids = (7, 8, 9, 10, 11, 12, 13, 14, 15)
+        self.lower_body_ids = (0, 1, 2, 3, 4, 5, 6)
+
+        self.num_cats = 19
+        self.cat_names = ['running', 'dancing', 'bicycling', 'walking', 'fishing and hunting', 'sport with ball',
+                           'standing', 'sitting', 'skiing', 'swimming', 'cooking', 'driving', 'rock climbing', 
+                           'horseback', 'skateboarding', 'yoga', 'canoe', 'training', 'lying']
+        
+        self.one_hot = self.get_one_hot()
+        
+        self.db = self._get_db()
+        self.db = self.select_data(self.db)
+
+        print('=> load {} samples'.format(len(self.db)))
+
+    def _get_db(self):
+
+        with open(self.ann_file) as anno_file:
+            anno = json.load(anno_file)
+
+        gt_db = []
+        for a in anno:
+            image_name = a['image']
+
+            center = np.array(a['center'], dtype=np.float)
+            scale = np.array([a['scale'], a['scale']], dtype=np.float)  
+            
+            assert len(self.cat_names) == self.num_cats, \
+                'cats num diff: {} vs {}'.format(len(self.cat_names),
+                                                  self.num_cats)
+            category = a['category']
+            assert category in self.cat_names, \
+                'no such cat name: {}'.format(category)
+            
+            one_hot_cat = self.one_hot[self.cat_names.index(category)]
+            
+            # Adjust center/scale slightly to avoid cropping limbs
+            if center[0] != -1:
+                center[1] = center[1] + 15 * scale[1]
+                # padding to include proper amount of context
+                scale = scale * 1.25
+
+            # MPII uses matlab format, index is 1-based,
+            # we should first convert to 0-based index
+            center = center - 1    
+
+            joints_2d = np.zeros((self.num_joints, 2), dtype=np.float32)
+            joints_2d_vis = np.zeros((self.num_joints,  2), dtype=np.float32)
+            
+            joints = np.array(a['joints'])
+            joints_vis = np.array(a['joints_vis'])
+            assert len(joints) == self.num_joints, \
+                'joint num diff: {} vs {}'.format(len(joints),
+                                                  self.num_joints)
+
+            joints_2d[:, 0:2] = joints[:, 0:2] - 1
+            joints_2d_vis[:, :2] = joints_vis[:, None]
+
+            gt_db.append(
+                {
+                    'image': image_name,
+                    'center': center,
+                    'scale': scale,
+                    'category': one_hot_cat,
+                    'joints_2d': joints_2d,
+                    'joints_2d_vis': joints_2d_vis,
+                }
+            )
+
+        return gt_db
+    
+    def get_one_hot(self):
+        return torch.nn.functional.one_hot(torch.arange(0, self.num_cats))
+    
